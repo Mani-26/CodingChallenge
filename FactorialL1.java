0cb5f1e69e09127dc4894e27f3433e7d02e347c1
Mani <maniinnovator26@gmail.com>
2023-04-07 19:44:51 +0530

    codes on friday 3

---
+++ FactorialL1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FactorialL1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int inp=sc.nextInt();
+        sc.close();
+        if (inp%2==0) {
+            
+        }
+    }
+}
---
+++ MaxString.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class MaxString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String inp=sc.nextLine();
+        int max=0;
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        if(inp.equals("-1"))
+            System.out.println("No string given");
+        else{
+            list1.add(inp);
+            while (true) {
+                inp=sc.nextLine();
+                if (inp.equals("-1")) {
+                    break;
+                }
+                else{
+                    list1.add(inp);
+                }
+            }
+            sc.close();
+            for(int i=0;i<list1.size();i++){
+                String str=list1.get(i);
+                int len=str.length();
+                if(max<len){
+                    max=len;
+                }
+            }
+            for (int i = 0; i < list1.size(); i++) {
+                String str=list1.get(i);
+                int len=str.length();
+                if (max==len) {
+                    list2.add(list1.get(i));
+                }
+            }
+            for (int i = 0; i < list2.size(); i++) {
+                System.out.print(list2.get(i)+" ");
+            }
+        }
+    }
+}
