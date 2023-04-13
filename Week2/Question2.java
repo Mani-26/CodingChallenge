a44af1edc42b153a04cf4c4c9bfa1b50b10402a3
Mani <maniinnovator26@gmail.com>
2023-04-13 23:22:54 +0530

    code week 2b

--- Week2/Question2.java
+++ Week2/Question2.java
@@ -3,6 +3,22 @@ import java.util.*;
 public class Question2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        String c=sc.nextLine();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int out=maths(c,a,b);
+        System.out.println(out);
+    }
+    public static int maths(String c, int a, int b) {
+        int res=0;
+        if(c.equals("+"))
+            res=a+b;
+        else if(c.equals("-"))
+            res= a-b;
+        else if(c.equals("/"))
+            res=a/b;
+        else if(c.equals("*"))
+            res=a*b;
+        return res;
     }
 }
